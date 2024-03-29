75f031c50d45eb6825b6e769abe6f4fdf4b56c1f
ziv <a0989698240@gmail.com>
2024-03-29 09:57:01 +0800

    first

---
+++ random_tarotcards/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.conf import settings
+import os
+import random
+from . import image_info
+
+
+def index(request):
+    images_dir = os.path.join(settings.BASE_DIR, 'static', 'images')
+    # 获取文件夹中所有图片的路径
+    images = [os.path.join(images_dir, image)
+              for image in os.listdir(images_dir)]
+    # print("Images in images_dir:", images)
+
+    if request.method == 'POST':
+        rws_selected = 'rws_images' in request.POST
+        cardname_selected = 'cardname' in request.POST
+        if request.POST.get('rws_images'):
+            images = [image for image in images if os.path.basename(
+                image).startswith('rws')]
+        # 如果是 POST 请求，随机选择一张图片
+        random_image = random.choice(images)
+        image_name = image_info.image_info_dict.get(os.path.basename(
+            random_image), {}).get('name', '')
+        print(image_name)
+        image_upright = image_info.image_info_dict.get(os.path.basename(
+            random_image), {}).get('upright', '')
+        image_reversed = image_info.image_info_dict.get(os.path.basename(
+            random_image), {}).get('reversed', '')
+        # print(random_image)
+        # print(rws_selected)
+        return render(request, 'index.html', {'image': random_image, 'rws_selected': rws_selected, 'name': image_name, 'upright': image_upright, 'reversed': image_reversed, 'cardname_selected': cardname_selected})
+
+    # 如果是 GET 请求，直接渲染模板
+
+    return render(request, 'index.html')
