9082c221341ee67a68c05307b2ba64d128405d1b
ziv <a0989698240@gmail.com>
2024-04-01 11:35:49 +0800

    update

--- random_tarotcards/views.py
+++ random_tarotcards/views.py
@@ -30,8 +30,10 @@ def index(request):
             random_image), {}).get('reversed', '')
 
         random_image = random_image.split('\\')[-2:]
-        # print(random_image[0]+'/'+random_image[1])
+
         random_image = random_image[0]+'/'+random_image[1]
+        print(random_image)
+
         return render(request, 'index.html', {'image': random_image, 'rws_selected': rws_selected, 'name': image_name, 'upright': image_upright, 'reversed': image_reversed, 'cardname_selected': cardname_selected})
 
     # 如果是 GET 请求，直接渲染模板
