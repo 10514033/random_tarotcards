ebf1679685e29fa4c28dd392db66353c0bfd19f5
ziv <a0989698240@gmail.com>
2024-04-02 13:25:17 +0800

    update

--- random_tarotcards/views.py
+++ random_tarotcards/views.py
@@ -6,7 +6,7 @@ from . import image_info
 
 
 def index(request):
-    images_dir = os.path.join(settings.BASE_DIR, 'static', 'file', 'images')
+    images_dir = os.path.join(settings.BASE_DIR, 'static', 'images')
     # 获取文件夹中所有图片的路径
     images = [os.path.join(images_dir, image)
               for image in os.listdir(images_dir)]
@@ -29,12 +29,10 @@ def index(request):
         image_reversed = image_info.image_info_dict.get(os.path.basename(
             random_image), {}).get('reversed', '')
 
-        random_image = random_image.split('\\')
-        print('/' + random_image[-3] + '/' + random_image[-2] +
-              '/' + random_image[-1])
+        # random_image = random_image.split('\\')[-1]
+        print(random_image)
 
-        return render(request, 'index.html', {'image': '/' + random_image[-3] + '/' + random_image[-2] +
-                                              '/' + random_image[-1], 'rws_selected': rws_selected, 'name': image_name, 'upright': image_upright, 'reversed': image_reversed, 'cardname_selected': cardname_selected})
+        return render(request, 'index.html', {'image': random_image, 'rws_selected': rws_selected, 'name': image_name, 'upright': image_upright, 'reversed': image_reversed, 'cardname_selected': cardname_selected})
 
     # 如果是 GET 请求，直接渲染模板
 
